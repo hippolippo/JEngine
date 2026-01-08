c5ca02a160d54246251c643dfd4279ea2c085d10
hippolippo <joelhaftel@gmail.com>
2026-01-07 19:19:57 -0500

    Added interpolation to rendering using affinetransform system

--- Drawing/LerpableTransform.java
+++ Drawing/LerpableTransform.java
@@ -14,9 +14,20 @@ public class LerpableTransform extends AffineTransform implements Lerpable<Lerpa
         super();
     }
 
+    public LerpableTransform(double m00, double m10, double m01, double m11, double m02, double m12){
+        super(m00, m10, m01, m11, m02, m12);
+    }
+
     @Override
     public LerpableTransform lerp(LerpableTransform other, double amount) {
-        return other;
+        return new LerpableTransform(
+            Lerpable.lerpDouble(getScaleX(), other.getScaleX(), amount),
+            Lerpable.lerpDouble(getShearY(), other.getShearY(), amount),
+            Lerpable.lerpDouble(getShearX(), other.getShearX(), amount),
+            Lerpable.lerpDouble(getScaleY(), other.getScaleY(), amount),
+            Lerpable.lerpDouble(getTranslateX(), other.getTranslateX(), amount),
+            Lerpable.lerpDouble(getTranslateY(), other.getTranslateY(), amount)
+        );
     }
     
 }
